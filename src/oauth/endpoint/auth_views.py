087bb1394624e5fabf070574a4d138146bafaa7a
speccy-rom <speccy.rom@gmail.com>
2021-11-03 21:45:53 +0300

    added: authorization in Spotify login

--- src/oauth/endpoint/auth_views.py
+++ src/oauth/endpoint/auth_views.py
@@ -4,23 +4,38 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 
 from .. import serializers
-from ..services.google import check_google_auth
+from ..services import google, spotify
 
 
 def google_login(request):
-    """Вход через Google
+    """Вход через Google.
     """
     context = {}
     return render(request, 'oauth/google_login.html', context)
 
 
+def spotify_login(request):
+    """Вход через spotify.
+    """
+    context = {}
+    return render(request, 'oauth/spotify_login.html', context)
+
+
 @api_view(['POST'])
 def google_auth(request):
-    """Подтверждение авторизации через Google
+    """Подтверждение авторизации через Google.
     """
     google_data = serializers.GoogleAuthSerializer(data=request.data)
     if google_data.is_valid():
-        token = check_google_auth(google_data.data)
+        token = google.check_google_auth(google_data.data)
         return Response(token)
     else:
-        return AuthenticationFailed(code=403, detail='Bad data Google')
+        return AuthenticationFailed(code=403, detail='Bad data Spotify')
+
+
+@api_view(['GET'])
+def spotify_auth(request):
+    """Подтверждение авторизации через Spotify.
+    """
+    token = spotify.spotify_auth(request.query_params.get('code'))
+    return Response(token)
--- src/oauth/urls.py
+++ src/oauth/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('me/', views.UserView.as_view({'get': 'retrieve', 'put': 'update'})),
 
     path("google/", auth_views.google_auth),
+    path("spotify-callback/", auth_views.spotify_auth),
+    path("spotify-login/", auth_views.spotify_login),
     path("", auth_views.google_login),
 ]
