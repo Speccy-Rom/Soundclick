337e024d995ca11596e1d174f74833ca744c39d9
speccy-rom <speccy.rom@gmail.com>
2021-11-09 22:40:32 +0300

    create: models for app (audio_library)

--- src/audio_library/urls.py
+++ src/audio_library/urls.py
@@ -1,7 +1,5 @@
 from django.urls import path
 
-
 urlpatterns = [
     # path("me/", views.UserView.as_view({"get": "retrieve", "put": "update"})),
-
 ]
--- src/base/services.py
+++ src/base/services.py
@@ -2,8 +2,23 @@ from django.core.exceptions import ValidationError
 
 
 def get_path_upload_avatar(instance, file):
-    """построение пути к файлу."""
+    """Построение пути к файлу format: (media)/avatar/user_id/photo.jpg."""
     return f"avatar/{instance.id}/{file}"
+
+
+def get_path_upload_cover_album(instance, file):
+    """Построение пути к файлу format: (media)/album/user_id/photo.jpg."""
+    return f"album/user_{instance.id}/{file}"
+
+
+def get_path_upload_track(instance, file):
+    """Построение пути к файлу format: (media)/track/user_id/photo.jpg."""
+    return f"track/user_{instance.id}/{file}"
+
+
+def get_path_upload_playlist(instance, file):
+    """Построение пути к файлу format: (media)/playlist/user_id/photo.jpg."""
+    return f"playlist/{instance.id}/{file}"
 
 
 def validate_size_image(file_obj):
