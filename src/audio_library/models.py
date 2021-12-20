703e7568da83ec0a119821a875afa82be6f2f974
speccy-rom <speccy.rom@gmail.com>
2021-12-20 23:21:45 +0300

    fixed: models app audio library

--- src/audio_library/models.py
+++ src/audio_library/models.py
@@ -2,13 +2,14 @@ from django.core.validators import FileExtensionValidator
 from django.db import models
 
 from src.base.services import (get_path_upload_cover_album,
-                               get_path_upload_playlist, get_path_upload_track,
-                               validate_size_image)
+                               get_path_upload_cover_playlist,
+                               get_path_upload_cover_track,
+                               get_path_upload_track, validate_size_image)
 from src.oauth.models import AuthUser
 
 
 class License(models.Model):
-    """Модель лицензий треков пользователя."""
+    """Модель лицензий треков пользователя"""
 
     user = models.ForeignKey(
         AuthUser, on_delete=models.CASCADE, related_name="licenses"
@@ -26,7 +27,7 @@ class Genre(models.Model):
 
 
 class Album(models.Model):
-    """Модель альбомов для треков."""
+    """Модель альбомов для треков"""
 
     user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name="albums")
     name = models.CharField(max_length=50)
@@ -44,7 +45,7 @@ class Album(models.Model):
 
 
 class Track(models.Model):
-    """Модель треков."""
+    """Модель треков"""
 
     user = models.ForeignKey(AuthUser, on_delete=models.CASCADE, related_name="tracks")
     title = models.CharField(max_length=100)
@@ -53,21 +54,33 @@ class Track(models.Model):
     )
     genre = models.ManyToManyField(Genre, related_name="track_genres")
     album = models.ForeignKey(Album, on_delete=models.SET_NULL, blank=True, null=True)
+    link_of_author = models.CharField(max_length=500, blank=True, null=True)
     file = models.FileField(
         upload_to=get_path_upload_track,
         validators=[FileExtensionValidator(allowed_extensions=["mp3", "wav"])],
     )
-    created_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField(auto_now_add=True)
     plays_count = models.PositiveIntegerField(default=0)
     download = models.PositiveIntegerField(default=0)
-    user_of_likes = (models.ManyToManyField(AuthUser, related_name="likes_of_tracks"),)
+    likes_count = models.PositiveIntegerField(default=0)
+    user_of_likes = models.ManyToManyField(AuthUser, related_name="likes_of_tracks")
+    private = models.BooleanField(default=False)
+    cover = models.ImageField(
+        upload_to=get_path_upload_cover_track,
+        blank=True,
+        null=True,
+        validators=[
+            FileExtensionValidator(allowed_extensions=["jpg"]),
+            validate_size_image,
+        ],
+    )
 
     def __str__(self):
         return f"{self.user} - {self.title}"
 
 
 class Comment(models.Model):
-    """Модель коментариев к треку."""
+    """Модель комментариев к треку"""
 
     user = models.ForeignKey(
         AuthUser, on_delete=models.CASCADE, related_name="comments"
@@ -76,11 +89,11 @@ class Comment(models.Model):
         Track, on_delete=models.CASCADE, related_name="track_comments"
     )
     text = models.TextField(max_length=1000)
-    created_at = models.DateTimeField(auto_now_add=True)
+    create_at = models.DateTimeField(auto_now_add=True)
 
 
-class Playlist(models.Model):
-    """Модель плейлистов пользователя."""
+class PlayList(models.Model):
+    """Модель плейлистов пользователя"""
 
     user = models.ForeignKey(
         AuthUser, on_delete=models.CASCADE, related_name="play_lists"
@@ -88,7 +101,7 @@ class Playlist(models.Model):
     title = models.CharField(max_length=50)
     tracks = models.ManyToManyField(Track, related_name="track_play_lists")
     cover = models.ImageField(
-        upload_to=get_path_upload_playlist,
+        upload_to=get_path_upload_cover_playlist,
         blank=True,
         null=True,
         validators=[
